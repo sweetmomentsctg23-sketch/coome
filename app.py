16a9a0eb67d77e22c337d84db9665374db34c948
sweetmomentsctg23-sketch <sweetmomentsctg23@gmail.com>
2026-09-16 13:05:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ TOKEN_BOT = "8075556042:AAFoz2S2xiLqDV_gEm0qc-HsxdbSNFm-nIM"
 # ==========================================
 @app.route('/')
 def index():
-    return redirect(url_for('index.html'))
+    return render_template('index.html')
 
 @app.route('/inicio')
 def vista_inicio():
